23fe90b6fb06bafef401b475a19a64013d42d81b
Caroline Oliveira <104214541+Dizziolica@users.noreply.github.com>
2022-12-15 11:54:53 +0000

    Create MergeList.py

---
+++ MergeList.py
@@ -0,0 +1,12 @@
+def MergeList(list1 , list2):
+    newlist = []
+    for i in list1:
+        
+        newlist.append(i)
+
+    for j in list2:
+        newlist.append(j)
+    print(newlist)
+MergeList([1,1,1,1,], [2,2,2,2])
+
+   
